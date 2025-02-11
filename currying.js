39ff554f16b8f34dea686341c052e312b61a82f2
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-02-10 20:02:54 -0400

    update currying function

--- currying.js
+++ currying.js
@@ -1,15 +1,21 @@
-// create currying function
-const curriedSum = curry((a, b, c) => a + b + c);
-const partiallyCurriedSum = curriedSum(1);
-console.log(partiallyCurriedSum(2, 3)); // 6
-
-function curry(cb) {
-  return function initCurried(...args) {
-    if (args.length >= cb.length) {
-      return cb(...args); // call callback function once all args are received
+function curry(fn) {
+  return function inner(...args) {
+    if (args.length >= fn.length) {
+      // check if provided args sufficient for what func expects
+      return fn(...args);
     }
-    return function subsequentCurrying(...addArgs) {
-      return cb(...args, ...addArgs); // collect the rest of arguments, call recursively
+    return (...newArgs) => {
+      // if not, take args, combine with existing and run recursively again
+      return inner(...args, ...newArgs);
     };
   };
 }
+
+const join = (a, b, c) => {
+  return `${a}_${b}_${c}`;
+};
+const curriedJoin = curry(join);
+const one = curriedJoin(1, 2, 3); // '1_2_3'
+const two = curriedJoin(1)(2, 3); // '1_2_3'
+const three = curriedJoin(1, 2)(3); // '1_2_3'
+console.log(one, two, three);
