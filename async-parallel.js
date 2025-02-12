bede0ed1093d87cb4d58c94632b64b1b9e4dccb0
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-02-12 13:50:25 -0400

    update async parallel

--- async-parallel.js
+++ async-parallel.js
@@ -1,9 +1,10 @@
 // Execute the given list of asynchronous functions in parallel and return the results as an array to the callback
 
-const asyncParallel = (fnArr, cb) => {
-  const promises = fnArr.map((fn) => new Promise((res) => fn(res)));
-  Promise.all(promises).then((result) => cb(result));
-};
+// BELOW IS IMPLEMENTATION FROM VIDEO:
+// const asyncParallel = (fnArr, cb) => {
+//   const promises = fnArr.map((fn) => new Promise((res) => fn(res)));
+//   Promise.all(promises).then((result) => cb(result));
+// };
 
 const asyncFn1 = (callback) => {
   // callback === res (from line 2)
@@ -26,3 +27,16 @@ const asyncFn3 = (callback) => {
 asyncParallel([asyncFn1, asyncFn2, asyncFn3], (result) => {
   console.log(result); // after 3 seconds all should display: 1, 2, 3
 });
+
+// PERSONAL IMPLEMENTATION:
+function promisify(fn) {
+  return new Promise((res) => {
+    fn((num) => res(num));
+  });
+}
+
+async function asyncParallel(funcArr, cb) {
+  const promises = funcArr.map((fn) => promisify(fn));
+  const result = await Promise.all(promises);
+  return cb(result);
+}
