c6713ace7193f6b65fee9ff44a0e949728ace90a
Vadym <vadym.hulchenko-ext@soundcloud.com>
2026-04-11 17:09:39 -0300

    feat: add default + custom loaders

---
+++ isr-mobx-nextjs/app/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div className="mt-16 w-full text-center">DEFAULT LOADING</div>;
+}
---
+++ isr-mobx-nextjs/components/customLoader.tsx
@@ -0,0 +1,3 @@
+export default function CustomLoader() {
+  return <div className="mt-16 w-full text-center">CUSTOM LOADING!</div>;
+}
